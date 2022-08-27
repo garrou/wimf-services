be53587aec05a0a258ce0c93e034c1ee958dfeb1
Adrien <39885692+1-irdA@users.noreply.github.com>
2022-08-27 11:18:47 +0200

    Change category image to varchar(255)

--- entities/category.go
+++ entities/category.go
@@ -3,5 +3,5 @@ package entities
 type Category struct {
 	ID    int    `gorm:"not null;"`
 	Name  string `gorm:"type:varchar(20);not null;"`
-	Image string `gorm:"type:varchar(50);not null;"`
+	Image string `gorm:"type:varchar(255);not null;"`
 }
